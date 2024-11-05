23e9faa321bddef04bd5ea084f642f5ade77af95
Domine Digital <dominedigitaldd@gmail.com>
2024-11-05 10:10:06 -0400

    project completed

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import { createDivForItems } from "./utils/creatDivForItems.js"
 
 const buttonSubmit = document.getElementById("button-submit-id")
+const inputText = document.getElementById("add-item-id")
+
+inputText.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault()
+    buttonSubmit.click()
+  }
+})
 
 buttonSubmit.addEventListener("click", (event) => {
   event.preventDefault()
 
-  const inputText = document.getElementById("add-item-id")
   const inputValueForDivCreation = inputText.value.trim()
-
-  console.log(inputValueForDivCreation)
 
   if (inputValueForDivCreation) {
     const divCreated = createDivForItems(inputValueForDivCreation)
@@ -24,7 +29,6 @@ buttonSubmit.addEventListener("click", (event) => {
 document.querySelector(".items").addEventListener("click", (event) => {
   if (event.target.classList.contains("img-trash")) {
     const parentDiv = event.target.parentElement
-    console.log("parentDiv", parentDiv)
 
     parentDiv.classList.remove("display-show")
     parentDiv.classList.add("display-none")
@@ -33,6 +37,10 @@ document.querySelector(".items").addEventListener("click", (event) => {
 
     disclaimer.classList.remove("display-none")
     disclaimer.classList.add("display-show")
+    setTimeout(() => {
+      disclaimer.classList.remove("display-show")
+      disclaimer.classList.add("display-none")
+    }, 2000)
   }
 })
 
